7d1149663b820a8595e4ab5be9ee0612f248f4ee
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2020-06-29 10:09:53 +0530

    Contentful

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { client } from "./client";
 import "./App.css";
-function App() {
-  return (
-    <div className="App">
-      <h1>Food Blog</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+    };
+  }
+  componentDidMount() {
+    client
+      .getEntries()
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Food Blog</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
